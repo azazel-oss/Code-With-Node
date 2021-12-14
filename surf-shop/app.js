e1ae4ad2b8ab12c2069076e7f1b033a5e2896505
Asad <asadmhmd1234@gmail.com>
2021-12-14 19:48:10 +0530

    Add post routes

--- surf-shop/app.js
+++ surf-shop/app.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 const app = express();
 
 // view engine setup
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
