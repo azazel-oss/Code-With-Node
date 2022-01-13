11c47bad6fe7bfb19366f77c723d8c380e03bb21
Asad <asadmhmd1234@gmail.com>
2022-01-13 18:41:26 +0530

    Add image edit and destroy code from cloudinary

--- surf-shop/controllers/posts.js
+++ surf-shop/controllers/posts.js
@@ -40,12 +40,43 @@ module.exports = {
   },
 
   async postUpdate(req, res, next) {
-    let post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+    let post = await Post.findById(req.params.id);
+
+    if (req.body.deleteImages && req.body.deleteImages.length) {
+      let deleteImages = req.body.deleteImages;
+      for (const public_id of deleteImages) {
+        await cloudinary.v2.uploader.destroy(public_id.trim());
+        for (const image of post.images) {
+          if (image.public_id === public_id.trim()) {
+            let index = post.images.indexOf(image);
+            post.images.splice(index, 1);
+          }
+        }
+      }
+    }
+    if (req.files) {
+      for (const file of req.files) {
+        let image = await cloudinary.v2.uploader.upload(file.path);
+        post.images.push({
+          url: image.secure_url,
+          public_id: image.public_id,
+        });
+      }
+    }
+    post.title = req.body.post.title;
+    post.description = req.body.post.description;
+    post.price = req.body.post.price;
+    post.location = req.body.post.location;
+    post.save();
+
     res.redirect(`/posts/${post.id}`);
   },
 
   async postDestroy(req, res, next) {
-    await Post.findByIdAndDelete(req.params.id);
+    let post = await Post.findByIdAndDelete(req.params.id);
+    for (const image of post.images) {
+      await cloudinary.v2.uploader.destroy(image.public_id);
+    }
     res.redirect("/posts");
   },
 };
